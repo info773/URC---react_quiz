cc01c2e7cc36ec973ab9343b36fd5dc1c891d325
“Berry” <info@felix-behrens.de>
2026-02-28 13:57:26 +0100

    Finished Section 16

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+// import "./styles.css";
+// import Challenge from "./Challenge";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Challenge /> */}
   </React.StrictMode>,
 );
